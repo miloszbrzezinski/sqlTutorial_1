ccefed21c58c048fdd45787fa7aefeafefb9ba85
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-18 19:47:57 +0200

    First tables added

---
+++ testScript.sql
@@ -0,0 +1,19 @@
+-- Write your own SQL object definition here, and it'll be included in your package.
+-- CREATE TABLE EmployeeDemographics
+-- (EmployeeID int,
+-- FirstName varchar(50),
+-- LastName varchar(50),
+-- Age int,
+-- Gender varchar(50))
+
+-- CREATE TABLE EmployeeSalary
+-- (
+--     EmployeeID int,
+--     JobTitle varchar(50),
+--     Salary int
+-- )
+
+INSERT INTO EmployeeDemographics VALUES
+(
+    1001, 'Jim', 'Halpert', 30, 'Male'
+)
