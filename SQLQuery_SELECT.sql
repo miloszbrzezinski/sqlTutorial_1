014bb58d4a47200ed20e7182b4c9e2743852173e
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-18 20:17:36 +0200

    basic SELECT queries

---
+++ SQLQuery_SELECT.sql
@@ -0,0 +1,37 @@
+-- Insert into EmployeeDemographics VALUES
+-- (1001, 'Jim', 'Halpert', 30, 'Male'),
+-- (1002, 'Pam', 'Beasley', 30, 'Female'),
+-- (1003, 'Dwight', 'Shrute', 29, 'Male'),
+-- (1004, 'Angela', 'Martin', 31, 'Female'),
+-- (1005, 'Toby', 'Flenderson', 32, 'Male'),
+-- (1006, 'Michael', 'Scott', 35, 'Male'),
+-- (1007, 'Meredith', 'Palmer', 32, 'Female'),
+-- (1008, 'Stanley', 'Hudson', 38, 'Male'),
+-- (1009, 'Kevin', 'Malone', 31, 'Male')
+
+-- INSERT INTO EmployeeSalary VALUES 
+-- (1001, 'Salesman', 45000),
+-- (1002, 'Recepcionist', 45000),
+-- (1003, 'Salesman', 45000),
+-- (1004, 'Accountant', 45000),
+-- (1005, 'HR', 45000),
+-- (1006, 'Regional Manager', 45000),
+-- (1007, 'Supplier Relations', 45000),
+-- (1008, 'Salesman', 45000),
+-- (1009, 'Accountant', 45000)
+
+-- SELECT * FROM EmployeeDemographics
+
+-- SELECT TOP 5 * FROM EmployeeDemographics
+
+-- SELECT DISTINCT(Gender) FROM EmployeeDemographics
+
+-- SELECT COUNT(LastName) AS LastNameCount FROM EmployeeDemographics
+
+-- SELECT MAX(Salary) FROM EmployeeSalary
+
+-- SELECT MIN(Salary) FROM EmployeeSalary
+
+-- SELECT AVG(Salary) FROM EmployeeSalary
+
+-- SELECT * FROM SQLtest.dbo.EmployeeSalary
