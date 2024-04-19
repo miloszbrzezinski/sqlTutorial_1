780e4d890607f5d0895371f74a6159c27b54fcfa
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-19 23:49:52 +0200

    UPDATING/DELETING  queries added

--- SQLQuery_UpdatingDeleting.sql
+++ SQLQuery_UpdatingDeleting.sql
@@ -1,1 +1,12 @@
 -- Write your own SQL object definition here, and it'll be included in your package.
+
+
+SELECT *
+FROM EmployeeDemographics
+
+-- UPDATE EmployeeDemographics
+-- SET EmployeeID = 1012
+-- WHERE FirstName = 'Holly' AND LastName = 'Flax'
+
+DELETE FROM EmployeeDemographics
+WHERE EmployeeID = 1010
